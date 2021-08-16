c62833d933e358f2f6e3f90b983e2006a7a3b6ee
Cleggatron <david.robert.clegg@gmail.com>
2021-08-16 21:42:50 +0100

    wrote basic get requests

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const PORT = process.env.PORT || 3001;
+const path = require("path");
 
 const app = express();
-const PORT = 3001;
 
+//return our notes file
+app.get("/notes", (req, res) =>
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+//handle any other requests with index.html
+app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "public/index.html"))
+)
 
 app.listen(PORT, () => 
     console.log(`Listening for requests on port ${PORT}.`)
