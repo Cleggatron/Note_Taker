86be8671f93d4b2668e14492fcdb4b54f9c56fde
Cleggatron <david.robert.clegg@gmail.com>
2021-08-21 22:16:51 +0100

    wrote post request

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
 //Routing for our API requests//
 const api = require("express").Router();
 const fs = require("fs");
-
+//import a random id generator
+const {v4 : uuidv4} = require("uuid");
 
 api.get("/", (req, res) =>{
     console.log(`${req.method} request received.`);
@@ -17,6 +18,39 @@ api.get("/", (req, res) =>{
 })
 
 api.post("/", (req, res) =>{
-    console.log("Post request recieved");
+    
+    const {title, text} = req.body;
+    
+    //validate our incoming note
+    if(!title || !text){
+        res.status(500).statusMessage("Not all the required data").end();
+    }else{
+        //create our new note obj
+        const newNote = {
+            id: uuidv4(),
+            title,
+            text   
+        }
+
+        fs.readFile("./db/db.json", "utf8", (err, data) =>{
+            if(err) {
+                console.error(err)
+            } else{
+                //update our file
+                const arrayData = JSON.parse(data);
+                arrayData.push(newNote);
+                //send data back for the note render
+                res.json(JSON.parse(data));
+
+                //update db.json
+                fs.writeFile("./db/db.json", JSON.stringify(arrayData, null, 4), err =>
+                    err ? console.error(err) : console.log("Data written to file.")
+                )
+            }
+        })
+    }
+    
+    
+
 })
 module.exports = api;
