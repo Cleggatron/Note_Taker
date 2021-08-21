d5689abb71e1038abe9c4a058ab7e8e4a96fa66e
Cleggatron <david.robert.clegg@gmail.com>
2021-08-21 23:07:31 +0100

    wrote delete request functionality

--- routes/api.js
+++ routes/api.js
@@ -37,20 +37,44 @@ api.post("/", (req, res) =>{
                 console.error(err)
             } else{
                 //update our file
-                const arrayData = JSON.parse(data);
-                arrayData.push(newNote);
+                const notesArray = JSON.parse(data);
+                notesArray.push(newNote);
                 //send data back for the note render
                 res.json(JSON.parse(data));
 
                 //update db.json
-                fs.writeFile("./db/db.json", JSON.stringify(arrayData, null, 4), err =>
+                fs.writeFile("./db/db.json", JSON.stringify(notesArray, null, 4), err =>
                     err ? console.error(err) : console.log("Data written to file.")
                 )
             }
         })
     }
-    
-    
+})
 
+api.delete("/:id", (req, res) => {
+    console.log("Delete request recieved");
+
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if(err){
+            console.error(err);
+            res.status(404).send("Notes not found").end();
+        }
+
+        const notesArray = JSON.parse(data)
+        
+        const updatedNotes = notesArray.filter(note => note.id !== req.params.id)
+
+        //return our updated notes to the file
+        res.json(updatedNotes);
+
+        //update our db.json file
+        fs.writeFile("./db/db.json", JSON.stringify(updatedNotes, null, 4), err =>
+        err ? console.error(err) : console.log("Note deleted from file.")
+    )
+
+
+    })
+    
+    
 })
 module.exports = api;
